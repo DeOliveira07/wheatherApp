92e227fe3b33802784ded51ac778b70886f70041
DeOliveira07 <mr3863801@gmail.com>
2026-07-24 16:25:01 -0300

    Parte 2: Manipulando elementos dinamicamente usando ViewModel

--- app/src/main/java/com/example/weatherapp/ui/HomePage.kt
+++ app/src/main/java/com/example/weatherapp/ui/HomePage.kt
@@ -15,7 +15,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomePage(modifier: Modifier = Modifier) {
+fun HomePage(
+    modifier: Modifier = Modifier,
+    viewModel: MainViewModel
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
