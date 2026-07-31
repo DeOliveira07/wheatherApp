51f31f01a6105509c72c10fa4167e05f5523715d
DeOliveira07 <mr3863801@gmail.com>
2026-07-31 15:40:34 -0300

    Parte 1: Preparacao e Configuracao dos Mapas

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    kotlin("plugin.serialization") version "2.0.21" apply false
+    kotlin("plugin.serialization") version "2.3.10" apply false
 }
