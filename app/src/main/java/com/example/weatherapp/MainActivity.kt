fa56d7ffb516d7d8cbfd4a532c9b1cd66107fe60
DeOliveira07 <mr3863801@gmail.com>
2026-08-07 16:59:45 -0300

    Parte 2: Realizando o Registro, Login e Logout de Usuários

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
@@ -22,60 +23,57 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import kotlin.OptIn
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.NavDestination.Companion.hasRoute
-import com.example.weatherapp.ui.nav.Route
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.CityDialog
 import com.example.weatherapp.ui.MainViewModel
 import com.example.weatherapp.ui.nav.BottomNavBar
 import com.example.weatherapp.ui.nav.BottomNavItem
 import com.example.weatherapp.ui.nav.MainNavHost
+import com.example.weatherapp.ui.nav.Route
+import com.example.weatherapp.ui.theme.Purple40
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels()
-
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+            val viewModel : MainViewModel by viewModels()
             var showDialog by remember { mutableStateOf(false) }
-
             val currentRoute = navController.currentBackStackEntryAsState()
             val showButton = currentRoute.value?.destination?.hasRoute(Route.List::class) == true
-            val launcher = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.RequestPermission(),
-                onResult = {}
-            )
+            val launcher = rememberLauncherForActivityResult(contract =
+                ActivityResultContracts.RequestPermission(), onResult = {} )
+
+            fun handleLogout() {
+                Firebase.auth.signOut()
+            }
 
             WeatherAppTheme {
-                if (showDialog) {
-                    CityDialog(
-                        onDismiss = { showDialog = false },
-                        onConfirm = { city ->
-                            if (city.isNotBlank()) {
-                                viewModel.add(city)
-                            }
-                            showDialog = false
-                        }
-                    )
-                }
-
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
                 Scaffold(
                     topBar = {
                         TopAppBar(
                             title = { Text("Bem-vindo/a!") },
                             actions = {
-                                IconButton(onClick = { finish() }) {
+                                IconButton(
+                                    onClick = { handleLogout() }
+                                ) {
                                     Icon(
-                                        imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                                        imageVector =
+                                            Icons.AutoMirrored.Filled.ExitToApp,
                                         contentDescription = "Localized description"
                                     )
                                 }
@@ -88,11 +86,14 @@ class MainActivity : ComponentActivity() {
                             BottomNavItem.ListButton,
                             BottomNavItem.MapButton,
                         )
-                        BottomNavBar(navController = navController, items = items)
+                        BottomNavBar(navController = navController, items)
                     },
                     floatingActionButton = {
-                        if (showButton) {
-                            FloatingActionButton(onClick = { showDialog = true }) {
+                        if(showButton) {
+                            FloatingActionButton(
+                                onClick = { showDialog = true },
+                                containerColor = Purple40
+                            ) {
                                 Icon(Icons.Default.Add, contentDescription = "Adicionar")
                             }
                         }
---
+++ app/src/main/java/com/example/weatherapp/ui/InputField.kt
@@ -0,0 +1,44 @@
+package com.example.weatherapp.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InputField(
+    value: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    isPassword: Boolean = false,
+    isError: Boolean = false,
+    errorMessage: String? = null,
+    modifier: Modifier
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        shape = RoundedCornerShape(16.dp),
+        isError = isError,
+        supportingText = {
+            if(isError && !errorMessage.isNullOrBlank()) {
+                Text(
+                    text = errorMessage,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        },
+        visualTransformation = if(isPassword) {
+            PasswordVisualTransformation()
+        } else {
+            VisualTransformation.None
+        }
+    )
+}
