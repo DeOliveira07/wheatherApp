9afeb324169f98b446e2389e6bd34d7b1c36b056
DeOliveira07 <mr3863801@gmail.com>
2026-08-07 17:47:06 -0300

    Parte 1: Preparação para usar o Firebase Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.8.2")
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
